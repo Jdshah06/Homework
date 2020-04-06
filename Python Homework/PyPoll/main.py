d152d0f071e654b298ada7dafbebacd4a3ba9581
Jay Shah <jayshah@Jays-MacBook-Pro.local>
2020-04-05 18:15:16 -0700

    Now Officially the Final Python Hw Push

--- Python Homework/PyPoll/main.py
+++ Python Homework/PyPoll/main.py
@@ -35,12 +35,10 @@ for row in rows:
     if row[2] not in unique_list:
         unique_list.append(row[2])
 
-#     candidate_votes[candidate_list]=0
 
-# candidate_votes[candidate_list]= candidate_votes[candidate_list] +1
 # print(unique_list)
 
-# print("___________________________________")
+# # print("___________________________________")
 
 candidate_count=[row[2] for row in rows]
 candidate_count= Counter(candidate_count)
@@ -50,14 +48,14 @@ khan_count =candidate_count['Khan']
 correy_count= candidate_count['Correy']
 li_count= candidate_count['Li']
 tooley_count= candidate_count["O'Tooley"]
-winner_count= [khan_count,correy_count,li_count,tooley_count,]
+winner_count= [khan_count,correy_count,li_count,tooley_count]
 
 # print(khan_count)
 # print(correy_count)
 # print(li_count)
 # print(tooley_count)
 
-# print("___________________________________")
+# # print("___________________________________")
 
 khan_average= khan_count/voter_count * 100
 # print("Khan Averages: "+ str(khan_average))
@@ -71,9 +69,23 @@ correy_averages= correy_count/voter_count * 100
 tooley_averages= tooley_count/voter_count * 100
 # print("tooley averages: " + str(tooley_averages))
 
-# print("------------------------------------------")
+# # print("------------------------------------------")
+
+# # winner= candidate_count == max(winner_count)
 
 
+def winner_function():
+    if khan_count == max(winner_count):
+        return "Winner: Khan"
+    elif li_count == max(winner_count):
+        return "Winner: Li"
+    elif correy_count == max(winner_count):
+        return "Winner: Correy"
+    # elif tooley_count == max(winner_count):
+    else:
+        return "Winner: O'tolley"
+
+# res = winner_function()
 
 print("Election Results")
 print("------------------------------------------")
@@ -84,5 +96,9 @@ print("Correy: " + str(format(correy_averages, '.2f')) + "% (" +str(correy_count
 print("Li: " + str(format(li_averages, '.2f')) + "% (" +str(li_count) + ")")
 print("O'Tooley: " + str(format(tooley_averages, '.2f')) + "% (" +str(tooley_count) + ")")
 print("------------------------------------------")
-print("Winner: Khan")
+# print("Winner: " + str(winnerFunction))
+# print("Winner: " , winnerFunction())
+# print("winner: " + str(max(winner_count)))
+print(winner_function())
 print("------------------------------------------")
+
